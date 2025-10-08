79ad3a679fab8e8cb2088536cdfe44b776e2af8f
BiiN12 <biniyameshete12@gmail.com>
2025-10-08 23:01:33 +0300

    - Added main game logic in main.ts for fetching and displaying Pokémon data. - Created a user interface with CSS for the Pokémon selection and battle mechanics.

---
+++ Random_Pokemon_Battle/src/type-object.ts
@@ -0,0 +1,66 @@
+
+
+interface PokemonTypes{
+    fire: string[];
+    water: string[];
+    electric: string[];
+    grass: string[];
+    ice: string[];
+    fighting: string[];
+    poison:string[];
+    ground:string[];
+    flying:string[];
+    psychic:string[];
+    bug:string[];
+    rock:string[];
+    ghost:string[];
+    dragon:string[];
+    dark:string[];
+    steel:string[];
+    fairy:string[];
+}
+
+export const superEffectivTypes: PokemonTypes = {
+    fire: ['grass', "ice", "bug","steel"],
+    water: ["fire","ground", "rock"],
+    electric: ["water", "flying"],
+    grass: ["water","ground", "rock"],
+    ice: ["grass", "ground", "flying", "dragon"],
+    fighting: ["normal", "ice", "rock", "dark", "steel"],
+    poison: ["grass", "fairy"],
+    ground: ["fire", "electric", "poison", "rock", "steel"],
+    flying: ["grass", "fighting", "bug"],
+    psychic: ["fighting", "poison"],
+    bug: ["grass", "psychic", "dark"],
+    rock: ["fire", "ice", "flying", "bug"],
+    ghost: ["psychic", "ghost"],
+    dragon: ["dragon"],
+    dark: ["psychic", "ghost"],
+    steel: ["ice", "rock"],
+    fairy: ["fighting", "dragon", "dark"]
+}
+
+
+
+export interface ChosenPokemonObject{
+    img: string;
+    name: string;
+    hp:number;
+    attack:number;
+    defense:number;
+    specialAttack:number;
+    specialDefense:number;
+    speed:number;
+    type:string[];
+}
+// export let chosenPokemon: ChosenPokemonObject = {
+//     img: '',
+//     name: '',
+//     hp:0,
+//     attack:0,
+//     defense:0,
+//     specialAttack:0,
+//     specialDefense:0,
+//     speed:0,
+//     type:[]
+// };
