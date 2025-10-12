a6e080a3ae8d680ec7aada677372c12c34cbfd76
TeaTimeJack <amitjack14@gmail.com>
2025-10-12 13:21:26 +0300

    deleted play game 1 from main

--- Random-Pokemon-Battle/src/main.ts
+++ Random-Pokemon-Battle/src/main.ts
@@ -1,11 +1,9 @@
 import './style.css'
-// import {playGame} from "./play-game";
 import {playGame2} from "./play-game-noloops.ts"
 
 const pokeBallImg = document.getElementById('pokemon-ball') as HTMLImageElement;
 pokeBallImg.addEventListener('click', () => { 
-    // playGame();
-    playGame2(1025);
+    playGame2(251);
 }, { once: true });
 
 
